5613225acbb1aac65c7f81dc40e0a7d2c9306b82
msauvage <47172455+msauvage@users.noreply.github.com>
2022-02-09 16:34:28 +0100

    Update MealPlanner.php
    Ajout des fonctions pour suggérer un nombre de calories au client

--- src/back/classes/business/model/MealPlanner.php
+++ src/back/classes/business/model/MealPlanner.php
@@ -155,4 +155,96 @@ class MeanPlanner extends Client{
         // $this->preferences_ingredients = $preferences_ingredients;
         super.setPreferencesCategories($preferences_ingredients);
     }
+    
+    /**
+    * DATE: 09/02/2022
+    * Developer: Matthieu SAUVAGEOT
+    
+    * Modifications:
+    *   - Ajout d'une fonction de suggestion d'un nombre de calories pour chaque jour
+    *   - Ajout d'une fonction pour calculer le Metabolisme d'un client
+    */
+    
+    /**
+    * Calcul du Métabolisme d'un client
+    * https://www.passionsante.be/index.cfm?fuseaction=art&art_id=13657
+    *
+    * Méthode de Calcul:
+    *   - Multiplier le poids (en kg) par 10
+    *   - Multiplier la taille (en cm) par 6.25
+    *   - Additionner les résultats (exemple : vous pesez 65 kg et vous mesurez 1,70 m : 650 + 1062,5 = 1712,5)
+    *   - Multiplier l'âge par 5 et retirez ce chiffre du résultat précédent (40 ans x 5 = 200, 1712,5 - 200 = 1512,5)
+    *   - Enfin, Retirer 161 pour une Femme OU Ajouter 5 pour un Homme
+    */
+    
+    /**
+    * Dans la classe Client, il y a les fonctions pour récupérer son poids, sa taille, son age et son genre (homme ou femme)
+    * Il y a aussi une fonction qui permet de connaître la pratique sportive du client (s'il pratique peu, un peu ou beaucoup d'exercice)
+    */
+    public metabolism($client){
+        $metabol = 0;
+        /**
+        * $metabol = ($client->getWeight())*10;
+        * $metabol += ($client->getSize())*6.25;
+        * $metabol -= ($client->getAge())*5;
+        */
+        
+        $metabol = super.getWeight()*10;
+        $metabol += super.getSize()*6.25;
+        * $metabol -= super.getAge()*5;
+        
+        // if($client->getGender() == 'woman'){
+        if(super.getGender() == 'woman'){
+            $metabol -= 161;
+        }
+        else{ // if(super.getGender() == 'man')
+            $metabol += 5;
+        }
+        
+        return $metabol;
+    }
+    
+    /**
+    * Suggestion de Calories
+    *
+    * Le nombre de calories suggérées est déterminé selon les attributs de l'utilisateur suivants:
+    *   - son Âge
+    *   - son Poids (en kg)
+    *   - sa Taille (en cm)
+    *   - son Genre (Homme ou Femme)
+    */
+    public function suggestedCalories($id){
+        foreach ($this->clients as $key => $client){
+            if($client-> getId() == $id){
+                /**
+                * Calcul des calories suggérées
+                * https://www.passionsante.be/index.cfm?fuseaction=art&art_id=13657
+                */
+                
+                // Calcul du métabolisme
+                $metabol = metabolism($client);
+                
+                /**
+                *   - Si le client ne pratique une actvité physique "Légère" (-30 minutes de marche à bon rythme par jour, au moins cinq jours par semaine):
+                *   multipliez votre métabolisme (MB) par 1.2.
+                *   - Si le client pratique une activité physique "Modérée", multiplier le MB par 1.55
+                *   - Si le client pratique une activité physique "Intense", multiplier le MB par 1.725.
+                */
+                if($client -> getActivity() == 'light'){
+                    $metabol *= 1.2;
+                }
+                elif($client -> getActivity() == 'moderate'){
+                    $metabol *= 1.55;
+                }
+                else{
+                    $metabol *= 1.725;
+                }
+                
+                print("\nNombre de calories suggérées pour le client",$id,":",$metabol);
+                
+                return True;
+            }
+        }
+        return False;
+    }
 }
