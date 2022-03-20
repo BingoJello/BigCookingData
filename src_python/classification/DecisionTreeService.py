dab3865036be63141c373abfa68f3bac81020dc1
Arthur Mimouni <arthur.mimouni@gmail.com>
2022-03-20 23:42:07 +0100

    suppression fichier

--- src_python/classification/DecisionTreeService.py
+++
@@ -1,9 +0,0 @@
-from ladon.ladonizer import ladonize
-import pickle
-
-# ecrire "python venv/Scripts/ladon-3.9-ctl.py testserve DecisionTreeService.py -p 8080" pour activer serveur SOAP python
-class DecisionTreeService(object) :
-    @ladonize(str, rtype=int)
-    def getCluster(self, ingredients) :
-        file_decision_tree = open("decisionTree", "rb")
-        decisionTree = pickle.load(file_to_read)
