249d39231ca2f3cc6911cd5679c4597651fb9aa8
iamdiallo <belladiallo145@gmail.com>
2022-03-26 23:31:06 +0100

    recipes inserted have been mixed 2500+

---
+++ data_traitement/DataClustering/DataClustering.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[292]:
+
+
+import json
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
+import seaborn as sns
+import plotly.graph_objs as go
+# uncommente this if you are using .ipynb
+# get_ipython().run_line_magic('run', 'DataBaseTraitement.ipynb')
+from DataBaseTraitement import persisetDB
+
+
+# In[293]:
+
+
+def readJson(file):
+    json_file = open(file, encoding='utf-8')
+    data = json_file.read()
+    recipes = json.loads(data)
+    json_file.close()
+    return recipes
+
+
+# In[294]:
+
+
+def getListIngredient(recipesList):
+    listIngre = []
+    cpt_ingredient = {}
+    frequence = 5
+    for index, recipe in enumerate(recipesList):
+        for i, ingredient in enumerate(recipe['ingredients']):
+            if ingredient['nom_ingre'] not in listIngre:
+                listIngre.append(ingredient['nom_ingre'])
+                cpt_ingredient[ingredient['nom_ingre']] = 1
+            else:
+                  cpt_ingredient[ingredient['nom_ingre']] += 1
+#     print("ingredient après suppression de doublons:",len(listIngre))
+    d = sorted(cpt_ingredient.items(), key=lambda x: x[1], reverse=True)
+#     print("ingredient et nombre d'appareance:",d)
+    for k,v in cpt_ingredient.items():
+# ["eau",'sel', "poivre","beure","farine","oeufs","huile d'olive","ail","oignon","lait","crème fraîche","oignons"]:
+        if v < frequence:
+            listIngre.remove(k)
+#     print("ingredient et nombre d'appareance 2:",len(listIngre)) 
+#     print("ingredient qui apparaissent plus de",frequence,":",len(listIngre))
+    tmp = recipesList   
+    for index, recipe in enumerate(recipesList):
+        listIngreTmp = []
+        for k, ingredient in enumerate(recipe['ingredients']):
+            listIngreTmp.append(ingredient['nom_ingre'])
+        if  any(x in listIngreTmp for x in listIngre) == False:
+            del tmp[index]
+    newTemp = []
+    for key, value in enumerate(tmp):
+        value['id_cluster'] = key
+    print("Recettes après traitement:",len(tmp))
+#     print("Recettes après traitement:",tmp)
+    return  listIngre,tmp
+
+
+# In[295]:
+
+
+def createVector(listIngredient, ListRecette):
+    dataset = []
+    listProvisoire = []
+    vectorSize = len(listIngredient)
+    recetteSize = len(ListRecette)
+    for i, recette in enumerate(ListRecette):
+        vector = np.zeros(vectorSize)
+        for index, ingredient in enumerate(recette['ingredients']):
+            listProvisoire.append(ingredient['nom_ingre'])
+            for k,listIngre in enumerate(listIngredient):
+                if listIngre in listProvisoire:
+                    vector[k]= 1
+            listProvisoire = []
+        dataset.append(vector)
+#         print(dataset)
+    return np.array(dataset)
+
+
+# In[296]:
+
+
+#Inertia measures how well a dataset was clustered by K-Means
+# It is calculated by measuring the distance between each data point and its centroid, 
+#squaring this distance, and summing these squares across one cluster.
+def chooseOptimalCluster(dataset):
+    sse = []
+    k_list = range(1, 15)
+    for k in k_list:
+        km = KMeans(n_clusters=k)
+        km.fit(dataset)
+        sse.append([k, km.inertia_])
+    oca_results_scale = pd.DataFrame({'Cluster': range(1,15), 'SSE': sse})
+    plt.figure(figsize=(12,6))
+    plt.plot(pd.DataFrame(sse)[0], pd.DataFrame(sse)[1], marker='o')
+    plt.title('Optimal Number of Clusters using Elbow Method (Scaled Data)')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
+    plt.show()
+
+
+# In[297]:
+
+
+def kmeans(dataset, n_clusters):
+    pca = PCA(70) # a changer
+    #Transform the data
+    dataset = pca.fit_transform(dataset)
+    print("len dataset",len(dataset))
+    kmeans = KMeans(n_clusters=n_clusters,init="random", random_state= 10)
+    label = kmeans.fit_predict(dataset)
+    centroids = kmeans.cluster_centers_
+    u_labels = np.unique(label)
+    plt.figure(figsize = (8,8))
+    for i in u_labels:
+        plt.scatter(dataset[label == i , 0] , dataset[label == i , 1] , label = i)
+    plt.scatter(centroids[:,0] , centroids[:,1] , s = 80, color="black")
+    plt.legend()
+    plt.show()
+    
+    dataReturn = []
+    for data in dataset:
+        data = data[:3]
+        dataReturn.append(data)
+    return kmeans,dataReturn
+
+
+# In[298]:
+
+
+# Using PCA to reduce the dataset into 3 principal 
+def pca(dataset, n_clusters):
+    kmeans_scale = KMeans(n_clusters=n_clusters, n_init=100, max_iter=400, init='k-means++', random_state=42).fit(dataset)
+    labels_scale = kmeans_scale.labels_
+    pca2 = PCA(n_components=70).fit(dataset)
+    pca2d = pca2.transform(dataset)
+    plt.figure(figsize = (10,10))
+    sns.scatterplot(pca2d[:,0], pca2d[:,1], 
+                    hue=labels_scale, 
+                    palette='Set1',
+                    s=100, alpha=0.2).set_title('KMeans Clusters (4) Derived from Original Dataset', fontsize=15)
+    plt.legend()
+    plt.ylabel('PC2')
+    plt.xlabel('PC1')
+    plt.show()
+    
+    Scene = dict(xaxis = dict(title  = 'PC1'),yaxis = dict(title  = 'PC2'),zaxis = dict(title  = 'PC3'))
+    labels = labels_scale
+    trace = go.Scatter3d(x=pca2d[:,0], y=pca2d[:,1], z=pca2d[:,2], 
+            mode='markers',marker=dict(color = labels, colorscale='Viridis', 
+            size = 10, line = dict(color = 'gray',width = 5)))
+    layout = go.Layout(margin=dict(l=0,r=0),scene = Scene, height = 1000,width = 1000)
+    data = [trace]
+    fig = go.Figure(data = data, layout = layout)
+    fig.show()
+
+
+# In[299]:
+
+
+#Feature Reduction via PCA
+def featureReduce(df_scale):
+    n_components = len(dataset[0])
+    pca = PCA(n_components=n_components)
+    pca_scale = pca.fit(df_scale)
+    print(pca_scale)
+    variance = pca.explained_variance_ratio_
+    var = np.cumsum(np.round(variance, 3)*100)
+    plt.figure(figsize=(12,6))
+    plt.ylabel('% Variance Explained')
+    plt.xlabel('# of Features')
+    plt.title('PCA Analysis')
+    plt.ylim(0,100.5)
+    plt.plot(var)
+    
+    
+    #check the variance ratio
+    pca = PCA(n_components=6)
+    pca_scale = pca.fit_transform(df_scale)
+    pca_df_scale = pd.DataFrame(pca_scale, columns=['pc1','pc2','pc3','pc4','pc5','pc6'])
+    print("pourcentage of the first PCAs")
+    print("we will use 70 features instead of 338")
+    print(pca.explained_variance_ratio_)
+    return pca_scale
+
+
+# In[300]:
+
+
+# Applying KMeans to PCA Principal Components
+def elbowPCA(pca_df_scale):
+    sse = []
+    k_list = range(1, 15)
+    for k in k_list:
+        km = KMeans(n_clusters=k)
+        km.fit(pca_df_scale)
+        sse.append([k, km.inertia_])
+
+    pca_results_scale = pd.DataFrame({'Cluster': range(1,15), 'SSE': sse})
+    plt.figure(figsize=(12,6))
+    plt.plot(pd.DataFrame(sse)[0], pd.DataFrame(sse)[1], marker='o')
+    plt.title('Optimal Number of Clusters using Elbow Method (PCA_Scaled Data)')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
+
+
+# In[301]:
+
+
+# Silhouette analysis can be used to study the separation distance between the resulting clusters
+def silhouette(pca_df_scale):
+    kmeans_pca_scale = KMeans(n_clusters=4, 
+    n_init=100, max_iter=400, init='k-means++', random_state=46).fit(pca_df_scale)
+    print('KMeans PCA Scaled Silhouette Score: {}'.format(silhouette_score(pca_df_scale, 
+    kmeans_pca_scale.labels_, metric='euclidean')))
+    labels_pca_scale = kmeans_pca_scale.labels_
+
+
+# In[302]:
+
+
+file = "test.json"
+ListRecetteRaw = readJson(file)
+print("le nombre de recettes raw",len(ListRecetteRaw))
+listIngredient,listRecette = getListIngredient(ListRecetteRaw)
+dataset = createVector(listIngredient, listRecette)
+pca_scale = featureReduce(dataset)
+elbowPCA(pca_scale)
+# silhouette(pca_scale)
+
+
+# In[303]:
+
+
+def get_ingredients(recipes):
+        dict = {}
+        for r in recipes:
+            for i in r.get('ingredients', []):
+                id = i.get('id_ingre')
+                if id in dict:
+                    dict[id][1] += 1
+                else:
+                    dict[id] = [i.get('nom_ingre'), 1]
+        return dict
+def get_ingr_rank(recipes):
+    dict = get_ingredients(recipes)
+    return sorted(dict.items(), key=lambda i: i[1][1], reverse=True)
+#         return {k: v for k, v in sorted(listIngre.items(), key=lambda item: item[1], reverse=True)}
+
+
+# In[304]:
+
+
+def linkCluster(kmeans,dataset,recipes,n_clusters):
+    recipes_db = []
+    for label in range(n_clusters):
+        label_index = [i for i, x in enumerate(kmeans.labels_) if x == label]
+#         recipes_cluster = [recipes[index] for index in label_index]
+        recipes_cluster = []
+        for index in label_index:
+            for recipe in recipes:
+                 if(recipe['id'] == index):
+                        recipes_cluster.append(recipe)
+        ingredients_ranked = get_ingr_rank(recipes_cluster)
+        print("\n\nCluster", label, ":")
+        ranking_string = ""
+        for index in range(len(ingredients_ranked)):
+            ingredient = ingredients_ranked[index]
+            if ingredient[1][1] != np.nan and ingredient[1][0] != np.nan:
+                ranking_string += ingredient[1][0] + " (" + str(ingredient[1][1]) + "), "
+        print(ranking_string + '\n')
+        for index_recipe in label_index:
+            for recipe in recipes:
+                if(recipe['id_cluster'] == index_recipe):
+                    recipes_db.append([recipe,label,dataset[index_recipe]])
+#                     print("Id recette:",recipe['id'],'\n')
+#                     print("Nom Recette:",recipe['nom'])
+#                     print("List Ingredients:")
+#                     for ingrediet in recipe['ingredients']:
+#                         print(ingrediet['nom_ingre'])
+    persisetDB(recipes_db)
+    print("recipes_db",len(recipes_db))
+
+
+# In[305]:
+
+
+n_clusters = 5
+k,d = kmeans(dataset,n_clusters)
+
+
+# In[306]:
+
+
+linkCluster(k,d,listRecette,n_clusters)
+
+
+# In[290]:
+
+
+pca(dataset,5)
+
+
+# In[291]:
+
+
+# Silhouette analysis can be used to study the separation distance between the resulting clusters
+def silhouette(dataset):
+    kmeans_scale = KMeans(n_clusters=5, n_init=100, max_iter=400, init='k-means++', random_state=42).fit(dataset)
+    print('KMeans Scaled Silhouette Score: {}'
+          .format(silhouette_score(dataset, kmeans_scale.labels_, metric='euclidean')))
+    labels_scale = kmeans_scale.labels_
+
+
+# In[ ]:
+
+
+silhouette(dataset)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
